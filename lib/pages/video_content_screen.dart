2182a5b069bc6d5ed0c4f396895c62b4e7bcc01d
izzetJmy <izzetjmy@gmail.com>
2023-04-07 18:49:12 +0300

    gradlde değişikler yapmak zorunda kaldım

--- lib/pages/video_content_screen.dart
+++ lib/pages/video_content_screen.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:akademi_yanimda/services/youtube_service.dart';
+import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class VideoContent extends StatefulWidget {
   const VideoContent({super.key});
@@ -9,8 +10,38 @@ class VideoContent extends StatefulWidget {
 }
 
 class _VideoContentState extends State<VideoContent> {
+  final videoURL = YoutubeService.flutter[1];
+   late YoutubePlayerController _controller;
+  @override
+  void initState() {
+    final videoID = YoutubePlayer.convertUrlToId(videoURL);
+    _controller = YoutubePlayerController(
+        initialVideoId: videoID!,
+        flags: YoutubePlayerFlags(autoPlay: false, mute: true, isLive: false,controlsVisibleAtStart: true));
+  }
+  @override
+  void dispose() {
+_controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Text("buraya geri butonu gelecek"),
+            SizedBox(
+              height: 10,
+            ),
+            YoutubePlayer(
+              controller: _controller,
+              showVideoProgressIndicator: true,
+              actionsPadding: EdgeInsets.all(10),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
