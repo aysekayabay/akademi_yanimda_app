b322cfe78581702eeed999f6f929613cd62a5150
izzetJmy <izzetjmy@gmail.com>
2023-04-09 16:04:58 +0300

    commit

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,9 +1,6 @@
 import 'package:akademi_yanimda/pages/main_page.dart/lessons_screen/lessons_screen.dart';
 import 'package:akademi_yanimda/pages/profile_screen.dart';
-<<<<<<< HEAD
-=======
 import 'package:akademi_yanimda/pages/ranking_screen.dart';
->>>>>>> 6a19560129b935fede3666d3ace106f032d4b4c1
 import 'package:akademi_yanimda/utilities/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
@@ -19,27 +16,14 @@ class HomeBarState extends State<HomeBar> {
   int _selectedIndex = 0;
   List<Widget> _widgetOptions = <Widget>[
     LessonsScreen(),
-<<<<<<< HEAD
-    Column(
-      children: [SizedBox(height: 100), Image(image: AssetImage("assets/images/Card.png"))],
-    ),
-=======
     RankingScreen(),
->>>>>>> 6a19560129b935fede3666d3ace106f032d4b4c1
     Text("Home2"),
     ProfileScreen(),
   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-<<<<<<< HEAD
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 18),
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-=======
       body: _widgetOptions.elementAt(_selectedIndex),
->>>>>>> 6a19560129b935fede3666d3ace106f032d4b4c1
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
         child: GNav(
--- lib/services/card_info.dart
+++ lib/services/card_info.dart
@@ -3,7 +3,6 @@ import 'package:google_fonts/google_fonts.dart';
 
 class CardInfo {
   static const String BASLIK = "Oyun ve Uygulama Akademisi";
-<<<<<<< HEAD
 
   static const List<String> DERS_ADI = [
     "Flutter",
@@ -24,15 +23,4 @@ class CardInfo {
       color: Colors.indigo, fontSize: 20, fontWeight: FontWeight.w500);
   static TextStyle KONU_ADI = GoogleFonts.rubik(
       color: Colors.indigo, fontSize: 18, fontWeight: FontWeight.w400);
-=======
-  static TextStyle BASLIK_STYLE = GoogleFonts.rubik(fontStyle: FontStyle.italic, color: Colors.white);
-  // static TextStyle AD_STYLE = GoogleFonts.rubik(color: Colors.white, fontWeight: FontWeight.w600, fontSize: 24);
-  static ButtonStyle CARD_BUTTON = ElevatedButton.styleFrom(backgroundColor: Colors.white, side: BorderSide(width: 5));
-  static TextStyle KONU_BASLIK = GoogleFonts.rubik(color: Colors.black87, fontSize: 24, fontWeight: FontWeight.w600);
-  static TextStyle KONU_BASLIK1 = GoogleFonts.rubik(color: Colors.indigo, fontSize: 20, fontWeight: FontWeight.w500);
-  static TextStyle KONU_ADI = GoogleFonts.rubik(color: Colors.indigo, fontSize: 18, fontWeight: FontWeight.w400);
-  static const List<String> DERS_ADI = ["Flutter", "Unity", "Girişimcilik", "İngilizce"];
-  static TextStyle AD_STYLE = TextStyle(fontFamily: "Poppins", fontSize: 18, fontWeight: FontWeight.w900);
- 
->>>>>>> 6a19560129b935fede3666d3ace106f032d4b4c1
 }
